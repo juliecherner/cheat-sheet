afd27f6c6700bcd69c494d02c3cf552a0fce384b
julie-chernen <juliechernen@gmail.com>
2023-02-01 21:19:22 +0200

    Problem solving patterns are added

--- index.js
+++ index.js
@@ -54,52 +54,144 @@ const someMysticMethod2 = (param) => {
   return resultVal.toUpperCase();
 };
 
-console.log(
-  "test1",
-  someMysticMethod("u 1uuui yoi777i7u t8e7re uoy7o8p81") ===
-    someMysticMethod2("u 1uuui yoi777i7u t8e7re uoy7o8p81")
-);
+// console.log(
+//   "test1",
+//   someMysticMethod("u 1uuui yoi777i7u t8e7re uoy7o8p81") ===
+//     someMysticMethod2("u 1uuui yoi777i7u t8e7re uoy7o8p81")
+// );
 
-console.log(
-  "test2",
-  someMysticMethod("aa aa55aaa 7a7aa 000a") ===
-    someMysticMethod2("aa aa55aaa 7a7aa 000a")
-);
+// console.log(
+//   "test2",
+//   someMysticMethod("aa aa55aaa 7a7aa 000a") ===
+//     someMysticMethod2("aa aa55aaa 7a7aa 000a")
+// );
 
-console.log("test3", someMysticMethod("") === someMysticMethod2(""));
-console.log("test4", someMysticMethod(" ") === someMysticMethod2(" "));
-console.log(
-  "test5",
-  someMysticMethod(" /ipokg66rgbrrje6666rbgo6uer rgurjr 8rghkberg6erggg") ===
-    someMysticMethod2(" /ipokg66rgbrrje6666rbgo6uer rgurjr 8rghkberg6erggg")
-);
-console.log(
-  "test6",
-  someMysticMethod(" /6uer yu r6 8rg 90jj op0 po99 5un m8") ===
-    someMysticMethod2(" /6uer yu r6 8rg 90jj op0 po99 5un m8")
-);
-console.log(
-  "test7",
-  someMysticMethod("a aa55 777 7a7aa 000a") ===
-    someMysticMethod2("a aa55 777 7a7aa 000a")
-);
+// console.log("test3", someMysticMethod("") === someMysticMethod2(""));
+// console.log("test4", someMysticMethod(" ") === someMysticMethod2(" "));
+// console.log(
+//   "test5",
+//   someMysticMethod(" /ipokg66rgbrrje6666rbgo6uer rgurjr 8rghkberg6erggg") ===
+//     someMysticMethod2(" /ipokg66rgbrrje6666rbgo6uer rgurjr 8rghkberg6erggg")
+// );
+// console.log(
+//   "test6",
+//   someMysticMethod(" /6uer yu r6 8rg 90jj op0 po99 5un m8") ===
+//     someMysticMethod2(" /6uer yu r6 8rg 90jj op0 po99 5un m8")
+// );
+// console.log(
+//   "test7",
+//   someMysticMethod("a aa55 777 7a7aa 000a") ===
+//     someMysticMethod2("a aa55 777 7a7aa 000a")
+// );
 
-console.log("test8", someMysticMethod("777") === someMysticMethod2("777"));
-console.log("test9", someMysticMethod("") === someMysticMethod2(""));
-console.log("test10", someMysticMethod("a") === someMysticMethod2("a"));
-console.log(
-  "test11",
-  someMysticMethod("88 8 888") === someMysticMethod2("88 8 888")
-);
-console.log(
-  "test12",
-  someMysticMethod("aa 888") === someMysticMethod2("aa 888")
-);
-console.log("test13", someMysticMethod("a 888") === someMysticMethod2("a 888"));
-console.log("test14", someMysticMethod("abc") === someMysticMethod2("abc"));
-console.log("test15", someMysticMethod("a a") === someMysticMethod2("a a"));
-console.log("test16", someMysticMethod("a abc") === someMysticMethod2("a abc"));
+// console.log("test8", someMysticMethod("777") === someMysticMethod2("777"));
+// console.log("test9", someMysticMethod("") === someMysticMethod2(""));
+// console.log("test10", someMysticMethod("a") === someMysticMethod2("a"));
+// console.log(
+//   "test11",
+//   someMysticMethod("88 8 888") === someMysticMethod2("88 8 888")
+// );
+// console.log(
+//   "test12",
+//   someMysticMethod("aa 888") === someMysticMethod2("aa 888")
+// );
+// console.log("test13", someMysticMethod("a 888") === someMysticMethod2("a 888"));
+// console.log("test14", someMysticMethod("abc") === someMysticMethod2("abc"));
+// console.log("test15", someMysticMethod("a a") === someMysticMethod2("a a"));
+// console.log("test16", someMysticMethod("a abc") === someMysticMethod2("a abc"));
 
-//console.log("printed result 2", someMysticMethod("hi you there uouo"));
-////console.log("printed result 3", someMysticMethod("you there uouo o"));
-//console.log("printed result 4", someMysticMethod("you there uouo oo"));
+function validAnagram(param1, param2) {
+  // add whatever parameters you deem necessary - good luck!
+  //
+  if (param1.length !== param2.length) return false;
+
+  const hashMap1 = getHashmap(param1);
+  const hashMap2 = getHashmap(param2);
+
+  for (const keyLetter in hashMap1) {
+    if (!hashMap2[keyLetter] || hashMap2[keyLetter] !== hashMap1[keyLetter])
+      return false;
+  }
+
+  return true;
+}
+
+const getHashmap = (string) => {
+  const hashMap = {};
+  for (const letter of string) {
+    hashMap[letter] = ++hashMap[letter] || 1;
+  }
+
+  return hashMap;
+};
+
+function countUniqueValues(array) {
+  if (array.length === 0) return 0;
+
+  let startPointer = 0;
+
+  for (let endPointer = 1; endPointer < array.length; endPointer++) {
+    if (array[startPointer] !== array[endPointer]) {
+      startPointer++;
+      array[startPointer] = array[endPointer];
+    }
+  }
+
+  return startPointer + 1;
+}
+// countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); // 7
+// countUniqueValues([1, 1, 1, 1, 1, 2]); // 2
+// countUniqueValues([]); // 0
+// countUniqueValues([-2, -1, -1, 0, 1]); // 4
+
+function search(array, val) {
+  let min = 0;
+  let max = array.length - 1;
+
+  while (min <= max) {
+    console.log("min-max", min, max);
+    let middle = Math.floor((min + max) / 2);
+    let currentElement = array[middle];
+    console.log("middle", middle);
+
+    if (currentElement < val) {
+      min = middle + 1;
+    } else if (currentElement > val) {
+      max = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+
+  return -1;
+}
+
+//console.log(search([1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 14, 67], 3));
+
+function averagePair(array, value) {
+  if (array.length < 2) return false;
+
+  let start = 0;
+  let end = array.length - 1;
+
+  while (start < end) {
+    const average = (array[start] + array[end]) / 2;
+    //console.log("average", average);
+    if (average == value) {
+      console.log("here it is true");
+      return true;
+    } else if (average < value) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+
+  return false;
+  // add whatever parameters you deem necessary - good luck!
+}
+
+console.log(averagePair([1, 2, 3], 2.5)); // true
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); // true
+console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)); // false
+console.log(averagePair([], 4)); // false
