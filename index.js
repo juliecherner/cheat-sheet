9b33405cfae8d07db0f3577c84e2adcf97f4b65c
julie-chernen <juliechernen@gmail.com>
2023-02-02 12:20:13 +0200

    Recursion is added

--- index.js
+++ index.js
@@ -191,7 +191,33 @@ function averagePair(array, value) {
   // add whatever parameters you deem necessary - good luck!
 }
 
-console.log(averagePair([1, 2, 3], 2.5)); // true
-console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); // true
-console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)); // false
-console.log(averagePair([], 4)); // false
+// console.log(averagePair([1, 2, 3], 2.5)); // true
+// console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); // true
+// console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)); // false
+// console.log(averagePair([], 4)); // false
+
+function isSubsequence(substring, seacrhString) {
+  let coincidenceAmount = 0;
+
+  // while (indexPointer < seacrhString.length) {
+  //   if (substring[coincidenceAmount] === seacrhString[indexPointer])
+  //     coincidenceAmount++;
+  //   if (coincidenceAmount === substring.length) return true;
+  //   indexPointer++;
+  // }
+  for (
+    let indexPointer = 0;
+    indexPointer < seacrhString.length;
+    indexPointer++
+  ) {
+    if (substring[coincidenceAmount] === seacrhString[indexPointer])
+      coincidenceAmount++;
+    if (coincidenceAmount === substring.length) return true;
+  }
+  return false;
+}
+
+console.log(isSubsequence("hello", "hello world"), "// true");
+console.log(isSubsequence("sing", "sting"), "// true");
+console.log(isSubsequence("abc", "abracadabra"), "// true");
+console.log(isSubsequence("abc", "acb"), "// false (order matters)");
