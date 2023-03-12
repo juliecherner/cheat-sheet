5c1cd5ad24aa5760347cf02a50f353ddcc78c33e
julie-chernen <juliechernen@gmail.com>
2023-03-12 15:20:06 +0200

    Merge sort algorithm is added

--- alrogithms.js
+++ alrogithms.js
@@ -54,3 +54,45 @@ function insertionSort(array) {
   }
   return arr;
 }
+
+//Merge sort / time complexity o(n log n), space: o(n)
+//array is splitted to 0 or 1 element and merged (sorted while merging)
+
+function mergeSortRecursion(unsortedArray) {
+  if (unsortedArray.length <= 1) return unsortedArray;
+
+  const midPoint = Math.floor(unsortedArray.length / 2);
+  const leftArr = unsortedArray.slice(0, midPoint);
+  const rightArr = unsortedArray.slice(midPoint);
+
+  return mergeTwoArrays(
+    mergeSortRecursion(leftArr),
+    mergeSortRecursion(rightArr)
+  );
+}
+
+function mergeTwoArrays(leftArr, rightArr) {
+  let resultArray = [],
+    leftIndex = 0,
+    rightIndex = 0;
+
+  while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+    if (leftArr[leftIndex] < rightArr[rightIndex]) {
+      resultArray.push(leftArr[leftIndex]);
+      leftIndex++;
+    } else {
+      resultArray.push(rightArr[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  if (leftArr[leftIndex]) {
+    var unaddedElements = leftArr.slice(leftIndex);
+    resultArray.push(...unaddedElements);
+  } else {
+    var unaddedElements = rightArr.slice(rightIndex);
+    resultArray.push(...unaddedElements);
+  }
+
+  return resultArray;
+}
